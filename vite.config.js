0b5badc899425d7516b5cede5edca490cec2be68
Guilherme Silva <sebastian_49@hotmail.com>
2024-11-20 19:21:14 -0400

    ajustamos o vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//   build: {
+//     outDir: 'dist',
+//   }
+// });
+
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      }
+    }
   }
 });
